68a925d46c6f6e81e5a9d423328b04361bf468da
Niemand21 <tobias_bauer2@web.de>
2021-11-19 11:41:42 +0100

    removed duplicates

--- script_01.js
+++ script_01.js
@@ -50,90 +50,40 @@ isJohnDoeEqual = (ageJohnDoe == ageJaneDoe); // == --> Sind Werte gleich?
 // console.log( (isJohnDoeOlder) ? "John is older" : "Jane is older");
 
 /************** IF - ELSE IF - ELSE ***************/
-// mit alternativen Fällen (älter, jpnger, gleich alt)
-
-if (isJohnDoeOlder) 
-{   
-        console.log("John Doe ist älter.");
-} 
-// 1. Alternative
-else if(isJohnDoeEqual)
-{               
-        console.log("John und Jane sind gleich alt");
-}
-// 2...n Alternative(n)
-        /*       
-        /*       
-        /*/
-// Alternative wenn alles nicht zutrifft --> last result. 
-else 
-{
-         console.log("John Doe ist jünger.");
-}
-
-// Deklaration + Assigment
-const ageJohn = 30; 
-const ageMark = 35; 
-
-// Deklaration
-let isJohnOlder,isJohnEqual;
-
-// Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
-
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-
-/************ IF  ************/
-// TINA --> There is no alternative!
-// entweder JA oder nix ... alternativlos
-
-// if (true) 
-// if (false) 
-// if(isJohnOlder)
-// if(ageJohn > ageMark)
-// {
-//     console.log("John ist älter.");
-// }
-
-/************ IF - ELSE  ************/
-// mit Alternative  
-// entweder ja oder nein
-
-// if (isJohnOlder)
-// {
-//     // Ja-Zweig / true
-//     console.log("John ist älter."); 
-// } 
-// else 
-// {
-//     // Nein-Zweig / false
-//     console.log("John ist jünger."); 
-// }
-
-/************  Ternäre (ternary) Schreibweise ************/
-// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
-
-/************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder) // John älter
-{
-    console.log("John ist älter."); 
-} 
-// 1. Alternative
-else if(isJohnEqual) // John gleich alt
-{
-    console.log("John ist gleich alt.");
-}
-// 2...n Alternative(n)
+// if (isJohnDoeOlder) 
+// {   
+//         console.log("John Doe ist älter.");
+// } 
+// // 1. Alternative
+// else if(isJohnDoeEqual)
+// {               
+//         console.log("John und Jane sind gleich alt");
+// }
+// // 2...n Alternative(n)
+//         /*       
+//         /*       
+//         /*/
+// // Alternative wenn alles nicht zutrifft --> last result. 
+// else 
+// {
+//          console.log("John Doe ist jünger.");
+// }
 
-// Alternative
-else  // John jünger
+
+const firstName = "Jane"
+let job; 
+
+job = "driver"; // .. fährt TAXI! / UBER
+
+switch (job) 
+
 {
-    console.log("John ist jünger."); 
+    case "driver":
+        console.log(firstName + " fährt TAXI!")
+        break;
+
+    default:
+        break;
 }
